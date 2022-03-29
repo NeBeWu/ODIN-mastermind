daa949c06c4430ddd75c8768a2214c423901e88b
Pedro <nebewu@gmail.com>
2022-03-29 09:37:04 -0300

    Add functionality to play as codemaker
    Refactor the players classes so both kind of players are
    able to compare codes. Implement a computer strategy to
    play as codebreaker by filtering possible guesses based
    on previous tries. Add the functionality to play as
    codemaker with the computer acting  as codebreaker.

--- mastermind.rb
+++ mastermind.rb
@@ -6,26 +6,46 @@ require_relative 'players'
 class Game
   def initialize
     @decoding_board = DecodingBoard.new
-    @codemaker = Codemaker.new
-    @codebreaker = Codebreaker.new
+    if choose_player == '1'
+      @codebreaker = Codebreaker.new('human')
+      @codemaker = Codemaker.new('computer')
+    else
+      @codebreaker = Codebreaker.new('computer')
+      @codemaker = Codemaker.new('human')
+    end
     @turn = -1
   end
 
+  def choose_player
+    puts 'Which player do you want to be? Enter 1 for codebreaker or 2 for codemaker'
+
+    player_option = gets.chomp
+
+    until validate_option(player_option)
+      puts 'Invalid input! Please enter 1 for codebreaker or 2 for codemaker.'
+      player_option = gets.chomp
+    end
+
+    player_option
+  end
+
   def play
-    puts 'Let\'s start!'
-    @codemaker.insert_random_code(@decoding_board.shielded_row)
+    puts 'Let\'s play mastermind!'
+    @codemaker.create_code(@decoding_board.shielded_row)
     play_turn until end_round?
     code_broken? ? puts('Congratulations! You broke the code!') : puts("Too bad! You couldn't break the code")
   end
 
   def play_turn
+    end_round?
+    @turn += 1
     codebreaker_input
     codemaker_output
     @decoding_board.show_board
   end
 
-  def codebreaker_input
-    puts 'Please insert your guess codebreaker.'
+  def codebreaker_input_human
+    puts "Please insert your code #{@codebreaker.name}."
     guess_code = gets.chomp
 
     until validate_code(guess_code)
@@ -36,6 +56,16 @@ class Game
     @codebreaker.insert_code(@decoding_board.guess_rows[@turn], guess_code)
   end
 
+  def codebreaker_input
+    if @codebreaker.type == 'computer'
+      guess_code = @codebreaker.filter_possible_guesses(@decoding_board.guess_rows[@turn - 1]).sample if @turn >= 1
+      guess_code = @codebreaker.possible_guesses.sample if @turn == 0
+      @codebreaker.insert_code(@decoding_board.guess_rows[@turn], guess_code.join)
+    else
+      codebreaker_input_human
+    end
+  end
+
   def codemaker_output
     black_keys = @codemaker.compute_black_code_entries(@decoding_board.guess_rows[@turn],
                                                        @decoding_board.shielded_row).length
@@ -44,6 +74,10 @@ class Game
 
     @codemaker.insert_keys(@decoding_board.guess_rows[@turn], black_keys, white_keys)
     @codemaker.reset_entries
+  end
+
+  def validate_option(option)
+    option.match?(/^[12]{1}$/)
   end
 
   def validate_code(code)
--- players.rb
+++ players.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Player
+  attr_reader :name
+  attr_accessor :type
+
+  def initialize(type)
+    @type = type
+    @name = self.class
+    @black_code_entries = []
+    @white_code_entries = []
+  end
+
   def insert_code(row, code)
     row.code = code.chars
   end
@@ -9,20 +19,6 @@ class Player
     random_code = generate_random_code
 
     insert_code(row, random_code)
-  end
-
-  private
-
-  def generate_random_code
-    Array.new(4).map { |_entry| %w[B R Y P O G].sample }.join
-  end
-end
-
-class Codemaker < Player
-  def initialize
-    super
-    @black_code_entries = []
-    @white_code_entries = []
   end
 
   def compute_black_code_entries(guess_row, shielded_row)
@@ -68,7 +64,59 @@ class Codemaker < Player
     @black_code_entries = []
     @white_code_entries = []
   end
+
+  private
+
+  def generate_random_code
+    Array.new(4).map { |_entry| %w[R G B Y O P].sample }.join
+  end
+end
+
+class Codemaker < Player
+  def create_code(shielded_row)
+    return insert_random_code(shielded_row) if @type == 'computer'
+
+    puts "Please insert your code #{@name}."
+    shielded_code = gets.chomp
+
+    until shielded_code.match?(/^[BRYPOG]{4}$/)
+      puts 'Invalid input! Please insert valid guess codebreaker.'
+      shielded_code = gets.chomp
+    end
+
+    insert_code(shielded_row, shielded_code)
+  end
 end
 
 class Codebreaker < Player
+  attr_accessor :possible_guesses
+
+  def initialize(type)
+    super
+    @possible_guesses = @type == 'computer' ? generate_possible_guesses : nil
+  end
+
+  def generate_possible_guesses
+    %w[R B G Y O P].product(%w[R B G Y O P], %w[R B G Y O P], %w[R B G Y O P])
+  end
+
+  def compute_possibility(guess_row, last_guess_row)
+    compute_black_code_entries(guess_row, last_guess_row)
+    filter_entries(guess_row, last_guess_row)
+    compute_white_code_entries(guess_row, last_guess_row)
+  end
+
+  def possible?(guess, last_guess_row)
+    @row = GuessRow.new
+    insert_code(@row, guess.join)
+
+    reset_entries
+    compute_possibility(last_guess_row, @row)
+    insert_keys(@row, @black_code_entries.length, @white_code_entries.length)
+    last_guess_row.keys == @row.keys
+  end
+
+  def filter_possible_guesses(last_guess_row)
+    possible_guesses.select! { |guess| possible?(guess, last_guess_row) }
+  end
 end
